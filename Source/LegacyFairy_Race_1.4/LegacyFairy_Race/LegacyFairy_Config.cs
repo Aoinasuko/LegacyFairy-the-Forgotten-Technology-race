f243475ffa3f369291d21bbe28204f73f47dd7cf
Aoi_Nasuko <aoinasuko.nasunasu@gmail.com>
2023-08-10 21:20:34 +0900

    0810

--- Source/LegacyFairy_Race_1.4/LegacyFairy_Race/LegacyFairy_Config.cs
+++ Source/LegacyFairy_Race_1.4/LegacyFairy_Race/LegacyFairy_Config.cs
@@ -29,7 +29,7 @@ namespace LegacyFairy_Race
 	public class LegacyFairy_Config : ModSettings
 	{
 		// バージョン
-		public static int ver = 201;
+		public static int ver = 210;
 
 		// アップデートバージョン
 		public static int Updatever = 0;
